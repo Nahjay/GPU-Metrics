65e0d3ab09154be5c76e26dfc5387d1a5d6db083
Nahjay <nahjaybattieste@gmail.com>
2024-02-08 11:44:16 -0500

    feat: commented out code for better readability

--- src/get_stats/get_stats.c
+++ src/get_stats/get_stats.c
@@ -12,12 +12,14 @@
 // Function to extract the frequency value of the GPU from the sysfs file
 int get_gpu_freq()
 {
+    // Open the file
     FILE *fp;
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
     int freq = 0;
 
+    // Create a file pointer and open the file
     fp = fopen("/sys/devices/gpu.0/devfreq/57000000.gpu/cur_freq", "r");
     if (fp == NULL)
     {
@@ -25,27 +27,32 @@ int get_gpu_freq()
         return -1;
     }
 
+    // Read the file and extract the frequency value
     while ((read = getline(&line, &len, fp)) != -1)
     {
         freq = atoi(line);
     }
 
+    // Close the file and free the memory
     fclose(fp);
     if (line)
         free(line);
 
+    // Return the frequency value
     return freq;
 }
 
 // Function to extract the temperature value of the GPU from the sysfs file
 int get_gpu_temp()
 {
+    // Open the file
     FILE *fp;
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
     int temp = 0;
 
+    // Create a file pointer and open the file
     fp = fopen("/sys/class/thermal/thermal_zone2/temp", "r");
     if (fp == NULL)
     {
@@ -53,15 +60,18 @@ int get_gpu_temp()
         return -1;
     }
 
+    // Read the file and extract the temperature value
     while ((read = getline(&line, &len, fp)) != -1)
     {
         temp = atoi(line);
     }
 
+    // Close the file and free the memory
     fclose(fp);
     if (line)
         free(line);
 
+    // Return the temperature value
     return temp;
 }
 
@@ -81,10 +91,15 @@ int truncate_gpu_freq(int freq)
 // Function to format the output of the GPU frequency and temperature into a string
 char *get_gpu_stats()
 {
+    // Get the GPU frequency and temperature
     int freq = get_gpu_freq();
     int temp = get_gpu_temp();
+
+    // Truncate the values
     freq = truncate_gpu_freq(freq);
     temp = truncate_gpu_temp(temp);
+
+    // Allocate memory for the string   
     char *stats = malloc(MAX_SIZE * sizeof(char));
 
     // Check if memeory allocation was successful
@@ -95,6 +110,8 @@ char *get_gpu_stats()
         return NULL;
     }
 
+
+    // Format the string and return it
     sprintf(stats, "GPU Frequency: %.1f GPU Temperature: %.1f ", freq, temp);
     return stats;
 }
